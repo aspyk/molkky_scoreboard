c20026b2a392153eb27c68c5c797229cbe02bd20
Remy FRANSEN <remy.fransen@artal.fr>
2024-09-23 12:36:35 +0200

    feat: improve number input with buttons

--- src/ConfigPage.tsx
+++ src/ConfigPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./styles/styles.css";
 
 interface ConfigPageProps {
   setTeams: (teams: string[]) => void;
@@ -43,16 +44,64 @@ export const ConfigPage: React.FC<ConfigPageProps> = ({
   return (
     <div>
       <h1>Configuration du jeu</h1>
-      <label>Nombre d'équipes : </label>
-      <input
-        type="number"
-        value={numTeams}
-        onChange={handleNumTeamsChange}
-        min="2"
-        max="10"
-      />
+      <div style={{ marginBottom: "8px" }}>
+        <label style={{ marginRight: "0px" }}>Nombre d'équipes : </label>
+
+        <button
+          onClick={() =>
+            handleNumTeamsChange({
+              target: { value: String(Math.max(2, numTeams - 1)) },
+            } as React.ChangeEvent<HTMLInputElement>)
+          }
+          style={{
+            padding: "5px",
+            margin: "0",
+            borderRadius: "4px 0px 0px 4px",
+            border: "1px solid #ccc",
+            width: "25px",
+            fontWeight: "bold",
+          }}
+        >
+          -
+        </button>
+
+        <input
+          type="number"
+          value={numTeams}
+          onChange={handleNumTeamsChange}
+          min="2"
+          max="10"
+          style={{
+            // fontSize: '1.5em',
+            // padding: '5px',
+            // width: '60px',
+            textAlign: "center",
+            margin: "0",
+            border: "1px solid #ccc",
+          }}
+        />
+
+        <button
+          onClick={() =>
+            handleNumTeamsChange({
+              target: { value: String(Math.min(10, numTeams + 1)) },
+            } as React.ChangeEvent<HTMLInputElement>)
+          }
+          style={{
+            padding: "5px",
+            margin: "0",
+            borderRadius: "0 4px 4px 0",
+            border: "1px solid #ccc",
+            width: "25px",
+            fontWeight: "bold",
+          }}
+        >
+          +
+        </button>
+      </div>
+
       {teamNames.map((name, index) => (
-        <div key={index}>
+        <div key={index} style={{ marginBottom: "8px" }}>
           <label>Nom de l'équipe {index + 1} : </label>
           <input
             type="text"
